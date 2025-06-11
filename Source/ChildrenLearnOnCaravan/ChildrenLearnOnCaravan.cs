3a0f49913bd8ad6259fd656ecbdae94494ef081b
Heather Athebyne <heather@athebyne.com>
2025-06-10 20:56:16 -0400

    initial commit

---
+++ Source/ChildrenLearnOnCaravan/ChildrenLearnOnCaravan.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using RimWorld;
+using HarmonyLib;
+using RimWorld.Planet;
+using Verse;
+
+namespace ChildrenLearnOnCaravan
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            //Harmony.DEBUG = true;
+            var harmony = new Harmony("heatherathebyne.childrenlearnoncaravan");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patch_Caravan_NeedsTracker
+    {
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(Caravan_NeedsTracker), "TrySatisfyPawnNeeds");
+        }
+        
+        static void Postfix(Pawn pawn)
+        {
+            if (ModsConfig.BiotechActive)
+            {
+                Need_Learning learning = pawn.needs.learning;
+                if (learning != null)
+                {
+                    pawn.needs.learning.Learn(1.2E-05f * pawn.GetStatValue(StatDefOf.LearningRateFactor) * 0.5f);
+                }
+            }
+            return;
+        }
+    }
+    }
